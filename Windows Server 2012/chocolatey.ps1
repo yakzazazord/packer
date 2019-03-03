05621f315b07a7c275117bf3274c1403df347a2b
yakzazazord <34948449+yakzazazord@users.noreply.github.com>
2019-03-03 21:56:41 +0100

    Add files via upload

---
+++ Windows Server 2012/chocolatey.ps1
@@ -0,0 +1,1 @@
+Set-ExecutionPolicy Bypass -force -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
---
+++ Windows Server 2012/firefox.ps1
@@ -0,0 +1,1 @@
+choco install -y -f firefox
